4cb5a185de32278e2428db38add8d866b24d939a
Lucas P <40347728+incapdns@users.noreply.github.com>
2026-01-03 06:22:35 -0300

    Implement onconnect event handler in worker

--- worker.js
+++ worker.js
@@ -1,1 +1,10 @@
-console.log(self.crossOriginIsolated)
+// worker.js
+self.onconnect = function(e) {
+  const port = e.ports[0];
+
+  port.onmessage = function(event) {
+    conn.postMessage(`Worker received: ${event.data} [${self.crossOriginIsolated}]`);
+  };
+
+  port.start();
+};
